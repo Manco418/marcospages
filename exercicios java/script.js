e3a773cbd085736a335ee37c907390d1044d5e19
Marcos  Freitas <126299389+Manco418@users.noreply.github.com>
2023-03-31 11:28:40 -0300

    Add files via upload

---
+++ exercicios java/script.js
@@ -0,0 +1,10 @@
+function carregar(){
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('img1')
+var date = new Date()
+var hora = data.getHours()
+msg.innerHTML='Agora são ${hora} horas.'
+}
+if('hora>=0 12&&hora<18'){
+    //BOM DIA!
+}else
